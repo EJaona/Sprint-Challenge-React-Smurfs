c5386a04b5cfc019c769d4ecfb3a9391c336d741
Enoka Jaona <Ejaona@gmail.com>
2019-01-25 10:38:28 -0600

    fetched data from api and set it to state. Added additional info to state for adding smurfs

--- village/src/App.js
+++ village/src/App.js
@@ -1,24 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
-import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
+import "./App.css";
+import SmurfForm from "./components/SmurfForm";
+import Smurfs from "./components/Smurfs";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       smurfs: [],
+      newSmurf: {
+        id: "",
+        name: "",
+        height: "",
+        age: ""
+      }
     };
   }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
+  componentDidMount = () => {
+    axios // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
+      .get("http://localhost:3333/smurfs")
+      .then(response => this.setState({ smurfs: response.data }))
+      .catch(error => console.log(error));
+  };
+
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <SmurfForm smurf={this.state.newSmurf} />
+        {this.state.smurfs.map(smurf => (
+          <Smurfs smurfs={this.state.smurfs} />
+        ))}
       </div>
     );
   }
